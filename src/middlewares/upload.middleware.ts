c3470ca15e850a427abc56f66b75b58e5d2d0db2
Sreejith <SSreedharan@acr.org>
2020-06-19 13:27:02 +0530

    bug fixes

--- src/middlewares/upload.middleware.ts
+++
@@ -1,6 +0,0 @@
-export function setUploadPath(uploadPath: any) {
-    return (req: { uploadPath: any; }, res: any, next: () => void) => {
-        req.uploadPath = uploadPath;
-        next();
-    }
-}
