52317feb5dab22b899c1986214dba0cd013ca194
Sreepriyavp <sreepriyav@nirasystems.com>
2020-06-19 14:12:26 +0530

    Removed auth controller

--- src/routes/Auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import AuthController from '../controllers/Auth.controller';
-
-const router = Router();
-
-// Create a new client
-router.post('/', [], AuthController.createUser);
-
-export default router;
